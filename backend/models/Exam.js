5605e69971c0c3f2d921081bd5f0b987d68e72a3
Abinaya S <abinayasakthivel61@gmail.com>
2025-10-15 10:50:16 +0530

    Update Exam.js

--- backend/models/Exam.js
+++ backend/models/Exam.js
@@ -2,18 +2,16 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 const Admin = require("./Admin");
 
-const Exam = sequelize.define(
-  "Exam",
-  {
-    exam_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    exam_name: { type: DataTypes.STRING(100), allowNull: false },
-    description: { type: DataTypes.STRING(255), allowNull: true },
-    exam_date: { type: DataTypes.DATEONLY, allowNull: false },
-    duration: { type: DataTypes.INTEGER, allowNull: false }, // in minutes
-    total_marks: { type: DataTypes.INTEGER, allowNull: false },
-    created_by: { type: DataTypes.INTEGER, references: { model: Admin, key: "admin_id" } }
-  },
-  { tableName: "exams", timestamps: false }
-);
+const Exam = sequelize.define("Exam", {
+  exam_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  exam_name: { type: DataTypes.STRING(100), allowNull: false },
+  description: { type: DataTypes.STRING(255), allowNull: true },
+  exam_date: { type: DataTypes.DATEONLY, allowNull: false },
+  duration: { type: DataTypes.INTEGER, allowNull: false },
+  total_marks: { type: DataTypes.INTEGER, allowNull: false },
+  created_by: { type: DataTypes.INTEGER, references: { model: Admin, key: "admin_id" } },
+  question_ids: { type: DataTypes.JSON, allowNull: true }
+}, { tableName: "exams", timestamps: false });
+
 
 module.exports = Exam;
